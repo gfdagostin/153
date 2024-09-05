f70ce571a1bdfcf81f89068d9f0ed0f117e992b5
gfdagostin <168616421+gfdagostin@users.noreply.github.com>
2024-09-05 10:51:14 -0300

    Add files via upload

---
+++ exercicio4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class exercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o dia: ");
+        int dia = scanner.nextInt();
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-Feira");
+                break;
+            case 3:
+                System.out.println("Terça-Feira");
+                break;
+            case 4:
+                System.out.println("Quarta-Feira");
+                break;
+            case 5:
+                System.out.println("Quinta-Feira");
+                break;
+            case 6:
+                System.out.println("Sexta-Feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Inválido");
+                break;
+        }
+    }
+}
